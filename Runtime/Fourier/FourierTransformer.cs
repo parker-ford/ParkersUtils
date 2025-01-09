0e629cf1979b233efbed3dd830217c864030ab71
parker-ford <35358924+parker-ford@users.noreply.github.com>
2025-01-08 18:55:25 -0800

    fourier transformer bones

--- Runtime/Fourier/FourierTransformer.cs
+++ Runtime/Fourier/FourierTransformer.cs
@@ -1,47 +1,64 @@
 
+using UnityEngine;
+
 namespace ParkersUtils
 {
 
     public enum FourierTransformerScaling
     {
-
+        None = 0,
     };
 
     public enum FourierTransformerShifting
     {
-
+        None = 0,
+        Centered = 1
     };
 
     public enum FourierTransformerAlgorithm
     {
-
+        FFT = 0,
+        DFT = 1
     };
 
-    public struct FourierTransformerSettings
+    public readonly struct FourierTransformerSettings
     {
-        FourierTransformerScaling scaling;
-        FourierTransformerShifting shifting;
-        FourierTransformerAlgorithm algorithm;
-    };
+        public FourierTransformerScaling Scaling { get; }
+        public FourierTransformerShifting Shifting { get; }
+        public FourierTransformerAlgorithm Algorithm { get; }
+
+        public FourierTransformerSettings(
+            FourierTransformerScaling scaling = default,
+            FourierTransformerShifting shifting = default,
+            FourierTransformerAlgorithm algorithm = default)
+        {
+            Scaling = scaling;
+            Shifting = shifting;
+            Algorithm = algorithm;
+        }
+    }
 
     public class FourierTransformer
     {
+        private readonly FourierTransformerSettings _settings;
+
+
         public FourierTransformer()
         {
-
+            _settings = new FourierTransformerSettings();
         }
 
         public FourierTransformer(FourierTransformerSettings settings)
         {
-
+            _settings = settings;
         }
 
-        public void Forward()
+        public void Forward(RenderTexture target)
         {
 
         }
 
-        public void Inverse()
+        public void Inverse(RenderTexture target)
         {
 
         }
