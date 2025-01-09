b0b7c73f1aafde42a7c8d962352129cfe1c890f0
parker-ford <35358924+parker-ford@users.noreply.github.com>
2025-01-08 17:58:18 -0800

    first test

---
+++ Runtime/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ParkersUtils
+{
+    public class Test : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Hello");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
